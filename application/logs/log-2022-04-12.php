6566bcf8567609bd1e63329411573df914e291ba
saka_mawa <sakadana2003@gmail.com>
2022-04-12 15:43:41 +0700

    Create log-2022-04-12.php

---
+++ application/logs/log-2022-04-12.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-04-12 15:41:40 --> 404 Page Not Found: /index
+ERROR - 2022-04-12 15:41:42 --> 404 Page Not Found: /index
+ERROR - 2022-04-12 15:41:42 --> 404 Page Not Found: /index
+ERROR - 2022-04-12 15:41:42 --> 404 Page Not Found: /index
+ERROR - 2022-04-12 15:41:44 --> 404 Page Not Found: /index
+ERROR - 2022-04-12 15:41:44 --> 404 Page Not Found: /index
+ERROR - 2022-04-12 15:41:44 --> 404 Page Not Found: /index
+ERROR - 2022-04-12 15:41:44 --> 404 Page Not Found: ../../modules/auth/controllers/Controller_ctl/serviceWorker.js
