191f976ffd834e3d223fc4af5140ad45bea098d0
saka_mawa <sakadana2003@gmail.com>
2022-04-14 14:42:43 +0700

    Update Profile

--- modules/profil/controllers/Controller_ctl.php
+++ modules/profil/controllers/Controller_ctl.php
@@ -14,8 +14,60 @@ class Controller_ctl extends MY_Frontend
 	{
 		// LOAD TITLE
 		$mydata['title'] = 'Profil';
+
+		// LOAD CSS
+		$this->data['css_add'][] = '<link rel="stylesheet" href="' . base_url('assets/css/style-dewa.css') . '">';
+
+		// LOAD JS
+		$this->data['js_add'][] = '<script src="' . base_url() . 'assets/js/page/auth/uploadfoto.js"></script>';
+
 		// LOAD VIEW
 		$this->data['content'] = $this->load->view('index', $mydata, TRUE);
+		$this->display($this->input->get('routing'));
+	}
+
+	public function ubah_profil()
+	{
+		// LOAD TITLE
+		$mydata['title'] = 'Ubah Profil';
+
+		// LOAD CSS
+		$this->data['css_add'][] = '<link rel="stylesheet" href="' . base_url('assets/css/style-dewa.css') . '">';
+
+		// LOAD VIEW
+		$this->data['content'] = $this->load->view('ubah_profil', $mydata, TRUE);
+		$this->display($this->input->get('routing'));
+	}
+
+	public function ubah_password()
+	{
+		// LOAD TITLE
+		$mydata['title'] = 'Ubah Kata Sandi';
+
+		// LOAD CSS
+		$this->data['css_add'][] = '<link rel="stylesheet" href="' . base_url('assets/css/style-dewa.css') . '">';
+
+		// LOAD JS
+		$this->data['js_add'][] = '<script src="' . base_url() . 'assets/js/page/auth/ubahpassword.js"></script>';
+
+		// LOAD VIEW
+		$this->data['content'] = $this->load->view('ubah_password', $mydata, TRUE);
+		$this->display($this->input->get('routing'));
+	}
+
+	public function bantuan()
+	{
+		// LOAD TITLE
+		$mydata['title'] = 'Bantuan';
+
+		// LOAD CSS
+		$this->data['css_add'][] = '<link rel="stylesheet" href="' . base_url('assets/css/style-dewa.css') . '">';
+
+		// LOAD JS
+		$this->data['js_add'][] = '<script src="' . base_url() . 'assets/js/page/auth/bantuan.js"></script>';
+
+		// LOAD VIEW
+		$this->data['content'] = $this->load->view('bantuan', $mydata, TRUE);
 		$this->display($this->input->get('routing'));
 	}
 }
