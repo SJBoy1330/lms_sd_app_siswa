e8dbfe9d01e0f7474c9899498cee2a7f9e26fc49
jeje <jjeje1303@gmail.com>
2022-04-15 12:34:04 +0700

    push

--- modules/profil/views/index.php
+++ modules/profil/views/index.php
@@ -49,13 +49,15 @@
                                 <i class="fa-solid fa-key-skeleton size-18"></i>
                             </div>
                         </div>
-                    </a>
-                    <a href="<?= base_url('profil/tentang_sekolah'); ?>" class="list-group-item border-0 bg-f5f5f5 shadow-none rounded-15 py-1 px-3 mb-3 mx-3">
-                        <div class="row p-2">
-                            <div class="d-flex col-auto align-items-center ">
-                                <div class="bg-ec3528 rounded-10 icon-box-profile d-flex justify-content-center align-items-center fs-3 text-light">
-                                    <i class="fa-solid fa-school size-18 "></i>
-                                </div>
+                        <div class="col align-self-center p-0">
+                            <p class="mb-0 fw-bold size-14">Ubah Kata Sandi</p>
+                            <p class="text-secondary size-12">
+                                perubahan kata sandi lama
+                            </p>
+                        </div>
+                        <div class="col-auto d-flex justify-content-end align-items-center ps-0">
+                            <div class="fs-3">
+                                <i class="fa-solid fa-chevron-right size-14"></i>
                             </div>
                         </div>
                     </div>
