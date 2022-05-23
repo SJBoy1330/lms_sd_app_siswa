1feb717dd1ca70c5eab29285933aa27762b510a0
saka_mawa <sakadana2003@gmail.com>
2022-05-23 12:49:50 +0700

    Update Terbaru

--- modules/auth/views/login.php
+++ modules/auth/views/login.php
@@ -25,12 +25,12 @@
             <form id="form_login" method="POST" url="<?= base_url('func_auth/login_proses'); ?>">
                 <div class="text-center my-5">
                     <p class="mb-1 title-1">Log in App Siswa</p>
-                    <p class="mb-0 fw-600 size-18 title-2">Masuk dengan username dan kata sandi</p>
+                    <p class="mb-0 fw-600 size-18 title-2">Masuk dengan kode sekolah, username dan kata sandi</p>
                 </div>
 
                 <div class="mb-4" id="req_kode_sekolah">
                     <label for="kode_sekolah" class="form-label title-3">Kode Sekolah</label>
-                    <input type="text" class="form-control form-control-pribadi" id="kode_sekolah" name="kode_sekolah" placeholder="Masukkan Username" autocomplete="off">
+                    <input type="text" class="form-control form-control-pribadi" id="kode_sekolah" name="kode_sekolah" placeholder="Masukkan Kode Sekolah" autocomplete="off">
                 </div>
 
                 <div class="mb-4" id="req_username">
