8326a9e393601c9be32e2a24a7f561827eac01ff
saka_mawa <sakadana2003@gmail.com>
2022-05-24 23:20:44 +0700

    Update

--- themes/main_frontend/javascript.php
+++ themes/main_frontend/javascript.php
@@ -1,7 +1,7 @@
 </div>
 </main>
 <!-- Footer -->
-<footer class="footer d-none">
+<footer class="footer">
     <div id="footer" class="container">
         <ul class="nav nav-pills nav-justified">
             <li class="nav-item">
